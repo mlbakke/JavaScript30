19f791fa8cc9915775a7eaeb84f917858bbb84fc
Martin Bakke <bakkemartin11@gmail.com>
2020-05-13 14:59:21 +0200

    drum kit: timing fixes
    fixed timing of the removal of 'playing'-class

--- 01 - JavaScript Drum Kit/drums.js
+++ 01 - JavaScript Drum Kit/drums.js
@@ -1,13 +1,15 @@
 window.addEventListener('keydown', function(k) {
 	const audio = document.querySelector(`audio[data-key="${k.keyCode}"]`);
 	const key = document.querySelector(`.key[data-key="${k.keyCode}"]`);
+	//Duration of the transition in CSS
+	const duration = 1000 * parseFloat(getComputedStyle(key).transitionDuration);
 
 	if (!audio) return; //if no audio associated with key, stop function
 	audio.currentTime = 0; //restart drum sample
-	key.classList.add('playing'); //add 'playing' visual-effect
-	audio.play(); //play drum sample
-	//remove play effect again after 50ms
+	key.classList.add('playing'); //add 'playing' visual-effect to key
+	audio.play();
+	//remove playing visual effect after transition is finished
 	setTimeout(function() {
 		key.classList.remove('playing');
-	}, 50);
+	}, duration);
 });
