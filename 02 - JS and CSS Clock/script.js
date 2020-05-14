3d0169b4da89a4902166da94a63af515eb8adc8a
Martin Bakke <bakkemartin11@gmail.com>
2020-05-14 15:16:24 +0200

    clock: functionality added, finished

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,1 +1,15 @@
-function setDate() {}
+function setDate() {
+	const now = new Date();
+	const seconds = now.getSeconds();
+	const minutes = now.getMinutes();
+	const hours = now.getHours();
+	const secondHand = document.querySelector('.second-hand');
+	const minuteHand = document.querySelector('.min-hand');
+	const hourHand = document.querySelector('.hour-hand');
+	//rotate hands based on seconds, minutes (/60 * 360) and hours (/12 * 360)
+	secondHand.style.transform = `rotate(${seconds * 6 + 90}deg)`;
+	minuteHand.style.transform = `rotate(${minutes * 6 + 90}deg)`;
+	hourHand.style.transform = `rotate(${hours * 30 + 90}deg)`;
+}
+//update clock each second
+setInterval(setDate, 1000);
