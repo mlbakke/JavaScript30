3cc1e176bca7a281e720d4f03dddd3e23db987b6
Martin Bakke <bakkemartin11@gmail.com>
2020-05-14 15:38:03 +0200

    clock: finished
    fixed transition-glitch when seconds are going from 60 to 0.

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -6,10 +6,20 @@ function setDate() {
 	const secondHand = document.querySelector('.second-hand');
 	const minuteHand = document.querySelector('.min-hand');
 	const hourHand = document.querySelector('.hour-hand');
+
+	//avoid glitch when new minute occurs
+	const hands = document.querySelectorAll('.hand');
+	if (seconds === 0) {
+		hands.forEach((hand) => (hand.style.transition = 'none'));
+	} else {
+		hands.forEach((hand) => (hand.style.transition = ''));
+	}
+
 	//rotate hands based on seconds, minutes (/60 * 360) and hours (/12 * 360)
 	secondHand.style.transform = `rotate(${seconds * 6 + 90}deg)`;
 	minuteHand.style.transform = `rotate(${minutes * 6 + 90}deg)`;
 	hourHand.style.transform = `rotate(${hours * 30 + 90}deg)`;
 }
+
 //update clock each second
 setInterval(setDate, 1000);
