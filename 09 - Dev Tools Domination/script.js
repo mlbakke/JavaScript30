9478da0a1fc0a39b71c99b2ae160004d118f24b0
Martin Bakke <bakkemartin11@gmail.com>
2020-05-21 13:22:40 +0200

    dev tools: finished

--- 09 - Dev Tools Domination/script.js
+++ 09 - Dev Tools Domination/script.js
@@ -7,25 +7,59 @@ function makeGreen() {
 }
 
 // Regular
+console.log('hei');
 
 // Interpolated
+console.log('Hello, I am a %s string %s!', 'cool', 'thing');
+//console.log(`Hello I am a ${varname} string ${var2name}!`)
 
 // Styled
+console.log('%cI am text', 'font-size:20px; background: lightgreen;');
 
 // warning!
+console.warn('WARNING, WARNING');
 
 // Error :|
+console.error('You broke it!');
 
 // Info
+console.info('Mane is beat');
 
 // Testing
+const p = document.querySelector('p');
+//throws assertion failed with msg if false, does nothing if true
+console.assert(p.classList.contains('list'), `That's not right!`);
 
 // clearing
+//clears console unless 'preserve log' is ticked
+console.clear();
 
 // Viewing DOM Elements
+console.dir(p);
 
 // Grouping together
+dogs.forEach((dog) => {
+	//alternatively .groupCollapsed (collapsed by default)
+	console.group(`${dog.name}`);
+	console.log(`This is ${dog.name}`);
+	console.log(`${dog.name} is ${dog.age} years old`);
+	console.log(`${dog.name} is ${dog.age * 7} dog years old`);
+	console.groupEnd(`${dog.name}`);
+});
 
 // counting
+console.count('Pete');
+console.count('Pete');
+console.count('Petter');
+console.count('Pete');
 
 // timing
+// Displays how long some operation(s) take
+console.time('fetching data');
+fetch('https://api.github.com/users/mlbakke').then((data) => data.json()).then((data) => {
+	console.timeEnd('fetching data');
+	console.log(data);
+});
+
+// table
+console.table(dogs);
