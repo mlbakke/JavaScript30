67004d555e35719e513d68c834af11fd86f8013e
Martin Bakke <bakkemartin11@gmail.com>
2020-05-22 18:01:08 +0200

    video: functionality finished
    todo: fullscreen button

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -27,10 +27,37 @@ function skip() {
 	video.currentTime += parseInt(time);
 }
 
+function handleRange() {
+	video[this.name] = this.value;
+}
+
+function handleProgress() {
+	const percent = video.currentTime / video.duration * 100;
+	progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+	if (!mousedown) {
+		return;
+	}
+
+	const scrubTime = e.offsetX / progress.offsetWidth * video.duration;
+	video.currentTime = scrubTime;
+}
+
 // event listeners
 toggle.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 skipButtons.forEach((button) => button.addEventListener('click', skip));
+ranges.forEach((range) => range.addEventListener('change', handleRange));
+//scrubbing
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', scrub);
+//check if mouse is pressed
+let mousedown = false;
+progress.addEventListener('mousedown', () => (mousedown = true));
+progress.addEventListener('mouseup', () => (mousedown = false));
