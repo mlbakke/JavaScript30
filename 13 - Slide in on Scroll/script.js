a81664941e7230625bc9dcac5acfa1aae66bd876
Martin Bakke <bakkemartin11@gmail.com>
2020-05-25 17:21:42 +0200

    scroll: finished

--- 13 - Slide in on Scroll/script.js
+++ 13 - Slide in on Scroll/script.js
@@ -13,3 +13,25 @@ function debounce(func, wait = 20, immediate = true) {
 		if (callNow) func.apply(context, args);
 	};
 }
+
+const slideImgs = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+	slideImgs.forEach((img) => {
+		// slide in img when it starts becoming visible
+		const slideInAt = window.scrollY + window.innerHeight - img.height / 4;
+		// bottom of img
+		const imgBottom = img.offsetTop + img.height;
+		const isHalfShown = slideInAt > img.offsetTop;
+		const isNotScrolledPast = window.scrollY < imgBottom;
+
+		// if image appears on page: slide in, else remove
+		if (isHalfShown && isNotScrolledPast) {
+			img.classList.add('active');
+		} else {
+			img.classList.remove('active');
+		}
+	});
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
