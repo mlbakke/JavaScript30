5b3bd5cbbc8bc0cf0413eb207e2043bb000d27a8
Martin Bakke <bakkemartin11@gmail.com>
2020-06-02 16:53:03 +0200

    tally: finished

--- 18 - Adding Up Times with Reduce/script.js
+++ 18 - Adding Up Times with Reduce/script.js
@@ -1,1 +1,17 @@
-const timeNodes = document.querySelectorAll('[data-time]');
+const timeNodes = [ ...document.querySelectorAll('[data-time]') ];
+
+const seconds = timeNodes
+	.map((node) => node.dataset.time)
+	.map((timeCode) => {
+		const [ mins, secs ] = timeCode.split(':').map(parseFloat);
+		return mins * 60 + secs;
+	})
+	.reduce((total, sec) => total + sec);
+
+let secondLeft = seconds;
+const hours = Math.floor(seconds / 3600);
+secondsLeft = secondsLeft % 3600;
+const mins = Math.floor(seconds / 60);
+secondsLeft = secondsLeft % 60;
+
+console.log(hours, mins, secondsLeft);
